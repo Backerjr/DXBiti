6bed80cdd0c1bc59c9aa7ea1690a1c9b09f7b697
Ahmed Backer <ahmedbackerjr@gmail.com>
2025-10-04 13:18:32 +0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import BoujeeBot from "@/components/BoujeeBot";
 export default function Home() {
   return (
     <main className="min-h-screen">
+      {/* Your site content */}
       <BoujeeBot />
     </main>
   );
